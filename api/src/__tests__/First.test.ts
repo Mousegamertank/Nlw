f36168039e53b559c4b2b7662447a1e9160534bc
Pedro Hnerique Silva <pedro.h.silva.marcal@gmail.com>
2021-02-28 15:40:58 -0300

    até a metade da 3 aula

---
+++ api/src/__tests__/First.test.ts
@@ -0,0 +1,9 @@
+//Intalar os Jests parei bo 39 3 aula
+
+describe("First, () => {
+
+    it("Must be able to som 2 numbers", () => {
+
+        expect(2 + 2).toBe(4);
+    });
+});
--- api/src/controllers/UserController.ts
+++ api/src/controllers/UserController.ts
@@ -1,15 +1,38 @@
 import { Request, Response } from "express";
-import { getRepository } from "typeorm";
-import { User } from "../models/User";
+import { getCustomRepository } from "typeorm";
+import { UserRepository } from "../repositories/UsersRespository";
 
 class UserController {
     async create(request: Request, response: Response) {
-        const body = request.body;
+        // const body = request.body;
+        // console.log(body);
+        // return response.send();
 
-        const userRepository = getRepository(User);
+        const { name, email } = request.body;
 
-        return response.send();
+        const userRepository = getCustomRepository(UserRepository);
+
+        //SELECT * FROM USERS WHERE EMAIL = "EMAIL"
+        const userAlreadyExist = await userRepository.findOne({
+            email
+        })
+
+        if (userAlreadyExist){
+            return response.status(400).json({
+                error: "User Already exists!",
+            });
+        }
+
+        const user = userRepository.create({
+         name, 
+         email
+        });
+
+        await userRepository.save(user);
+       
+        return response.json(user);
+       
     }
 }
 
-export{ UserController }
+export { UserController };
